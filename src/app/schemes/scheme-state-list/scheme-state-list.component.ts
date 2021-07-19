35796c9145a43a80bd0600438625dd32d02d58c3
Maksim Kavsadze <maxim1548@gmail.com>
2021-07-19 13:18:54 +0600

    Task 81: Join scheme state icons.

--- src/app/schemes/scheme-state-list/scheme-state-list.component.ts
+++ src/app/schemes/scheme-state-list/scheme-state-list.component.ts
@@ -1,19 +1,13 @@
-import {Component, Input, OnChanges, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Connection_State, Scheme} from '../../user';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-scheme-state-list',
   templateUrl: './scheme-state-list.component.html',
-  styleUrls: ['./scheme-state-list.component.css']
+  styleUrls: ['./scheme-state-list.component.css', '../schemes-list.css']
 })
-export class SchemeStateListComponent implements OnInit, OnChanges {
-
-  status_weight = {
-    '1': 1,
-    '2': 2,
-    '3': 3,
-    '4': 4
-  };
-
+export class SchemeStateListComponent {
   status_class = {
     '1': 'ok',
     '2': 'undef',
@@ -24,47 +18,88 @@ export class SchemeStateListComponent implements OnInit, OnChanges {
   isModalOpen = false;
 
   get most_bad_status() {
-    const mbs = this.messages.reduce((acc, cur) => acc = cur.status > acc ? cur.status : acc, [1]);
-
-    //console.log(mbs);
-    return mbs;
+    return this.scheme?.messages?.reduce((acc, cur) => acc = cur.status > acc ? cur.status : acc, [1]);
   }
 
-  @Input() messages: any;
+  get isSchemeConnected(): boolean {
+      let connected;
+      switch (this.scheme.connect_state & 0b111) {
+          case Connection_State.CS_CONNECTED:
+          case Connection_State.CS_CONNECTED_JUST_NOW:
+          case Connection_State.CS_CONNECTED_SYNC_TIMEOUT:
+              connected = true;
+              break;
+          case Connection_State.CS_DISCONNECTED:
+          case Connection_State.CS_DISCONNECTED_JUST_NOW:
+          case Connection_State.CS_SERVER_DOWN:
+              connected = false;
+      }
+
+      return connected;
+  }
+
+  @Input() scheme: Scheme;
 
   constructor(
+      private translate: TranslateService,
   ) { }
-
-  ngOnInit() {
-    this.update();
-  }
-
-  ngOnChanges() {
-    this.update();
-  }
-
-  update() {
-    if (!this.messages) {
-      this.messages = [];
-    }
-/*
-    console.log(this.messages);
-
-    this.most_bad_status = 1;
-
-    for (const st of this.messages) {
-      const stweight =  this.status_weight[st.status];
-
-      if (stweight > this.most_bad_status) {
-        this.most_bad_status = stweight;
-      }
-    }
-    */
-  }
 
   toggleModal(e: any) {
       this.isModalOpen = !this.isModalOpen;
       e.stopPropagation();
   }
 
+  public status_desc(): string {
+        let result = '';
+
+        if (this.scheme.mod_state) {
+            result += this.translate.instant('MODIFIED') + '. ';
+        }
+
+
+        if (this.scheme.loses_state) {
+            result += 'С потерями пакетов. '; // TODO: translation
+        }
+
+        if (this.scheme.status_checked) {
+            switch (this.scheme.connect_state) {
+                case Connection_State.CS_SERVER_DOWN:
+                    return this.translate.instant('SERVER_DOWN');
+                case Connection_State.CS_DISCONNECTED:
+                    return result + this.translate.instant('OFFLINE');
+                case Connection_State.CS_CONNECTED:
+                    return result + this.translate.instant('ONLINE');
+                case Connection_State.CS_CONNECTED_MODIFIED:
+                    return result + this.translate.instant('MODIFIED');
+                case Connection_State.CS_DISCONNECTED_JUST_NOW:
+                    return result + this.translate.instant('DISCONNECTED_JUST_NOW');
+                case Connection_State.CS_CONNECTED_JUST_NOW:
+                    return result + this.translate.instant('CONNECTED_JUST_NOW');
+                case Connection_State.CS_CONNECTED_SYNC_TIMEOUT:
+                    return result + this.translate.instant('CONNECTED_SYNC_TIMEOUT');
+            }
+        }
+        return this.translate.instant('WAIT') + '...';
+    }
+
+    public get_status_class(): string { // TODO: remove this duplicate
+        if (!this.scheme.status_checked) {
+            return 'status_check';
+        }
+
+        if (this.scheme.mod_state) {
+            return 'status_modified';
+        }
+
+        switch (this.scheme.connect_state) {
+            case Connection_State.CS_CONNECTED_SYNC_TIMEOUT:
+            //  return 'status_sync_fail';
+            case Connection_State.CS_CONNECTED_MODIFIED:
+                return 'status_modified';
+            case Connection_State.CS_DISCONNECTED_JUST_NOW:
+                return 'status_bad_just';
+            case Connection_State.CS_CONNECTED_JUST_NOW:
+                return 'status_sync';
+        }
+    }
 }
